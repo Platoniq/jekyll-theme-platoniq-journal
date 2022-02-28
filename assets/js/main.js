9666c50d42e3a7755def1a3a009b30c685431898
Vera Rojman <vrojman@protonmail.com>
2022-02-28 18:48:57 +0100

    Add gallery js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,26 @@
 console.log("jekyll-theme-platoniq-journal");
 
-$(document).ready(function(){
+$(document).ready(function() {
   var $btn = $(".header__locale-selector button.burguer");
   var $menu = $(".menu__locale-selector");
 
   $btn.click(function(e) {
-    $btn.toggleClass( "status-open" );
-    $menu.toggleClass( "status-open" );
+    $btn.toggleClass("status-open");
+    $menu.toggleClass("status-open");
   });
+
+  var $gallery = $(".pj-gallery .pj-gallery--images");
+
+  $galleryBtnPrevious = $(".pj-gallery--button-previous");
+  $galleryBtnNext = $(".pj-gallery--button-next");
+
+  // https://www.jqueryscript.net/slider/Fully-Responsive-Flexible-jQuery-Carousel-Plugin-slick.html
+  $gallery.slick({
+    prevArrow: $galleryBtnPrevious.prop("outerHTML"),
+    nextArrow: $galleryBtnNext.prop("outerHTML"),
+    infinite: false
+  });
+
+  $galleryBtnPrevious.detach();
+  $galleryBtnNext.detach();
 });
