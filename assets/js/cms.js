16fb627972524e532b94d6e93464ba078a5c02e4
Vera Rojman <vrojman@protonmail.com>
2022-04-11 17:09:43 +0200

    (feat): author tag (#63)
    * WIP
    
    * Update author tag
    
    * Bump version

--- assets/js/cms.js
+++ assets/js/cms.js
@@ -11,6 +11,7 @@ scripts:
   - editor_component_links_tag.liquid
   - editor_component_numbers_tag.liquid
   - editor_component_questions_tag.liquid
+  - editor_component_author_tag.liquid
   - editor_component_videos.liquid
 ---
 
