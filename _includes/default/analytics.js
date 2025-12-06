5e8993300fd9f2e3cd6fce2c2d60ddd284c61f82
Harrison Hassig <harrisonhassig@gmail.com>
2025-12-06 08:22:16 +0100

    Add matomo tracking

--- _includes/default/analytics.js
+++ _includes/default/analytics.js
@@ -1,11 +1,26 @@
-// Analytics code
+// Google Analytics code
 var analytics_id = "{{ site.analytics_id }}";
 
-window.dataLayer = window.dataLayer || [];
+if (analytics_id) {
+  window.dataLayer = window.dataLayer || [];
 
-$.getScript("https://www.googletagmanager.com/gtag/js?id=" + analytics_id, function() {
-  function gtag() { dataLayer.push(arguments); }
+  $.getScript("https://www.googletagmanager.com/gtag/js?id=" + analytics_id, function() {
+    function gtag() { dataLayer.push(arguments); }
 
-  gtag('js', new Date());
-  gtag('config', analytics_id);
-});
+    gtag('js', new Date());
+    gtag('config', analytics_id);
+  });
+}
+
+// Matomo Analytics
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="//analytics.platoniq.net/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '8']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
